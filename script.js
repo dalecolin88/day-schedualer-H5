02d7b462f63ff1ce57eb67ca4da498cad3b52db8
Colin Dale <dalecolin88@gmail.com>
2021-04-01 08:30:50 -0700

    header update

---
+++ script.js
@@ -0,0 +1,21 @@
+var nowTime = $("#currentDay");
+
+// console.log({ currentTime });
+var dynaTime = setInterval(function () {
+  var currentDay = moment();
+  nowTime.text(
+    "the time is currently"
+    //   moveTime +
+    //   " on " +
+    //   currentTime.format("HH:mm:ss") +
+    //   " on " +
+    //   currentTime.format("yyyy/mm/dd")
+  ),
+    1000;
+});
+
+var currentTime = currentTime.format ();
+var moveTime = currentTime.format();
+
+var hi = $("openModal");
+
