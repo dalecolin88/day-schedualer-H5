a00983c518b72fe5023656d85561f28361f5b739
Colin Dale <dalecolin88@gmail.com>
2021-04-01 18:09:37 -0700

    upated html

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,20 @@
-var nowTime = $("#currentDay"); {
+var nowTime = $("#currentDay");
 
 // console.log({ currentDay });
-var dynaTime = setInterval(function(){
-  var currentDay = moment();
-    nowTime.text("Todays Time" + currentDay.format("hh:mm:ss")+ "on" + currentDay.format("MM-DD-YYYY"))
-    },1000)
-}
+var dynaTime = setInterval(function () {
+    var currentDay = moment();
+    nowTime.text("Todays Time " + currentDay.format("hh:mm:ss") + " on " + currentDay.format("MM-DD-YYYY"))
+}, 1000)
+console.log(currentDay)
 
 
 
-var hi = $("openModal");
+
+// var hi = $("openModal");
+
+var saveBtn = $(".save-icon")
+
+$(".save-icon").on("click", function () {
+
+})
 
